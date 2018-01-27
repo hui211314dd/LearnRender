288e810f11a4e3ee716ce72b1ace0bc4be3db8c6
PC-201710062131\Administrator <843566933@qq.com>
2018-01-27 18:15:08 +0800

    增加数学库

--- mini3d.c
+++ mini3d.c
@@ -7,6 +7,250 @@
 
 typedef unsigned int IUINT32;
 
+//数学库，矩阵向量定义以及运算等
+typedef struct { float m[4][4]; } matrix_t;
+typedef struct { float x, y, z, w; } vector_t;
+typedef vector_t point_t;
+
+//区间限制
+int CMID(int x, int min, int max) { return x < min ? min : (x > max ? max : x); }
+
+//线性插值，t的区间为[0,1]
+float interp(float x1, float x2, float t) { return x1 + (x2 - x1)*t; }
+
+//向量的模
+float vector_length(const vector_t* v)
+{
+	float sq = v->x*v->x + v->y*v->y + v->z*v->z;
+	return (float)sqrt(sq);
+}
+
+//向量加法
+void vector_add(vector_t* ret,const vector_t* v1, const vector_t* v2)
+{
+	ret->x = v1->x + v2->x;
+	ret->y = v1->y + v2->y;
+	ret->z = v1->z + v2->z;
+	ret->w = 1;
+}
+
+//向量减法
+void vector_sub(vector_t* ret, const vector_t* v1, const vector_t* v2)
+{
+	ret->x = v1->x - v2->x;
+	ret->y = v1->y - v2->y;
+	ret->z = v1->z - v2->z;
+	ret->w = 1;
+}
+
+//向量点乘
+float vector_dotproduct(const vector_t*v1, const vector_t* v2)
+{
+	return v1->x*v2->x + v1->y*v2->y + v1->z*v2->z;
+}
+
+//向量叉乘
+void vector_crossproduct(vector_t* z, const vector_t* x, const vector_t* y)
+{
+	float m1, m2, m3;
+	m1 = x->y * y->z - x->z * y->y;
+	m2 = x->z * y->x - x->x * y->z;
+	m3 = x->x * y->y - x->y * y->x;
+	z->x = m1;
+	z->y = m2;
+	z->z = m3;
+	z->w = 1;
+}
+
+// 矢量插值，t取值 [0, 1]
+void vector_interp(vector_t *z, const vector_t *x1, const vector_t *x2, float t) 
+{
+	z->x = interp(x1->x, x2->x, t);
+	z->y = interp(x1->y, x2->y, t);
+	z->z = interp(x1->z, x2->z, t);
+	z->w = 1.0f;
+}
+
+// 矢量归一化
+void vector_normalize(vector_t *v) {
+	float length = vector_length(v);
+	if (length != 0.0f) {
+		float inv = 1.0f / length;
+		v->x *= inv;
+		v->y *= inv;
+		v->z *= inv;
+	}
+}
+
+// c = a + b
+void matrix_add(matrix_t *c, const matrix_t *a, const matrix_t *b) {
+	int i, j;
+	for (i = 0; i < 4; i++) {
+		for (j = 0; j < 4; j++)
+			c->m[i][j] = a->m[i][j] + b->m[i][j];
+	}
+}
+
+// c = a - b
+void matrix_sub(matrix_t *c, const matrix_t *a, const matrix_t *b) {
+	int i, j;
+	for (i = 0; i < 4; i++) {
+		for (j = 0; j < 4; j++)
+			c->m[i][j] = a->m[i][j] - b->m[i][j];
+	}
+}
+
+// c = a * b
+void matrix_mul(matrix_t *c, const matrix_t *a, const matrix_t *b) 
+{
+	matrix_t z;
+	int i, j;
+	for (i = 0; i < 4; i++) {
+		for (j = 0; j < 4; j++) {
+			z.m[j][i] = (a->m[j][0] * b->m[0][i]) +
+				(a->m[j][1] * b->m[1][i]) +
+				(a->m[j][2] * b->m[2][i]) +
+				(a->m[j][3] * b->m[3][i]);
+		}
+	}
+	*c = z; //c[0] = z;
+}
+
+// c = a * f 矩阵乘以标量
+void matrix_scale(matrix_t *c, const matrix_t *a, float f) {
+	int i, j;
+	for (i = 0; i < 4; i++)
+	{
+		for (j = 0; j < 4; j++)
+			c->m[i][j] = a->m[i][j] * f;
+	}
+}
+
+// y = x * m 向量为行向量，左乘矩阵;
+void matrix_apply(vector_t *y, const vector_t *x, const matrix_t *m) {
+	float X = x->x, Y = x->y, Z = x->z, W = x->w;
+	y->x = X * m->m[0][0] + Y * m->m[1][0] + Z * m->m[2][0] + W * m->m[3][0];
+	y->y = X * m->m[0][1] + Y * m->m[1][1] + Z * m->m[2][1] + W * m->m[3][1];
+	y->z = X * m->m[0][2] + Y * m->m[1][2] + Z * m->m[2][2] + W * m->m[3][2];
+	y->w = X * m->m[0][3] + Y * m->m[1][3] + Z * m->m[2][3] + W * m->m[3][3];
+}
+
+//设置为单位矩阵
+void matrix_set_identity(matrix_t *m) {
+	m->m[0][0] = m->m[1][1] = m->m[2][2] = m->m[3][3] = 1.0f;
+	m->m[0][1] = m->m[0][2] = m->m[0][3] = 0.0f;
+	m->m[1][0] = m->m[1][2] = m->m[1][3] = 0.0f;
+	m->m[2][0] = m->m[2][1] = m->m[2][3] = 0.0f;
+	m->m[3][0] = m->m[3][1] = m->m[3][2] = 0.0f;
+}
+
+//设置为零矩阵
+void matrix_set_zero(matrix_t *m) {
+	m->m[0][0] = m->m[0][1] = m->m[0][2] = m->m[0][3] = 0.0f;
+	m->m[1][0] = m->m[1][1] = m->m[1][2] = m->m[1][3] = 0.0f;
+	m->m[2][0] = m->m[2][1] = m->m[2][2] = m->m[2][3] = 0.0f;
+	m->m[3][0] = m->m[3][1] = m->m[3][2] = m->m[3][3] = 0.0f;
+}
+
+// 平移变换
+void matrix_set_translate(matrix_t *m, float x, float y, float z) {
+	matrix_set_identity(m);
+	m->m[3][0] = x;
+	m->m[3][1] = y;
+	m->m[3][2] = z;
+}
+
+// 缩放变换
+void matrix_set_scale(matrix_t *m, float x, float y, float z) {
+	matrix_set_identity(m);
+	m->m[0][0] = x;
+	m->m[1][1] = y;
+	m->m[2][2] = z;
+}
+
+
+// 旋转矩阵,通过四元数构建
+void matrix_set_rotate(matrix_t *m, float x, float y, float z, float theta) {
+	float qsin = (float)sin(theta * 0.5f);
+	float qcos = (float)cos(theta * 0.5f);
+	vector_t vec = { x, y, z, 1.0f };
+	float w = qcos;
+	vector_normalize(&vec);
+	x = vec.x * qsin;
+	y = vec.y * qsin;
+	z = vec.z * qsin;
+	m->m[0][0] = 1 - 2 * y * y - 2 * z * z;
+	m->m[1][0] = 2 * x * y - 2 * w * z;
+	m->m[2][0] = 2 * x * z + 2 * w * y;
+	m->m[0][1] = 2 * x * y + 2 * w * z;
+	m->m[1][1] = 1 - 2 * x * x - 2 * z * z;
+	m->m[2][1] = 2 * y * z - 2 * w * x;
+	m->m[0][2] = 2 * x * z - 2 * w * y;
+	m->m[1][2] = 2 * y * z + 2 * w * x;
+	m->m[2][2] = 1 - 2 * x * x - 2 * y * y;
+	m->m[0][3] = m->m[1][3] = m->m[2][3] = 0.0f;
+	m->m[3][0] = m->m[3][1] = m->m[3][2] = 0.0f;
+	m->m[3][3] = 1.0f;
+}
+
+// 旋转矩阵,通过基向量构建
+void matrix_set_rotate2(matrix_t* pOut, float x, float y, float z, float theta)
+{
+	float a = 0;
+	float b = 0;
+	float c = 0;
+
+	vector_t p;
+	p.x = x;
+	p.y = y;
+	p.z = z;
+	p.w = 1;
+
+	vector_normalize(&p);
+	float u = p.x;
+	float v = p.y;
+	float w = p.z;
+
+	float uu = u * u;
+	float uv = u * v;
+	float uw = u * w;
+	float vv = v * v;
+	float vw = v * w;
+	float ww = w * w;
+	float au = a * u;
+	float av = a * v;
+	float aw = a * w;
+	float bu = b * u;
+	float bv = b * v;
+	float bw = b * w;
+	float cu = c * u;
+	float cv = c * v;
+	float cw = c * w;
+
+	float costheta = cosf(theta);
+	float sintheta = sinf(theta);
+
+	pOut->m[0][0] = uu + (vv + ww) * costheta;
+	pOut->m[0][1] = uv * (1 - costheta) + w * sintheta;
+	pOut->m[0][2] = uw * (1 - costheta) - v * sintheta;
+	pOut->m[0][3] = 0;
+
+	pOut->m[1][0] = uv * (1 - costheta) - w * sintheta;
+	pOut->m[1][1] = vv + (uu + ww) * costheta;
+	pOut->m[1][2] = vw * (1 - costheta) + u * sintheta;
+	pOut->m[1][3] = 0;
+
+	pOut->m[2][0] = uw * (1 - costheta) + v * sintheta;
+	pOut->m[2][1] = vw * (1 - costheta) - u * sintheta;
+	pOut->m[2][2] = ww + (uu + vv) * costheta;
+	pOut->m[2][3] = 0;
+
+	pOut->m[3][0] = (a * (vv + ww) - u * (bv + cw)) * (1 - costheta) + (bw - cv) * sintheta;
+	pOut->m[3][1] = (b * (uu + ww) - v * (au + cw)) * (1 - costheta) + (cu - aw) * sintheta;
+	pOut->m[3][2] = (c * (uu + vv) - w * (au + bv)) * (1 - costheta) + (av - bu) * sintheta;
+	pOut->m[3][3] = 1;
+}
+
 //=====================================================================
 // Win32 窗口及图形绘制：为 device 提供一个 DibSection 的 FB
 //=====================================================================
